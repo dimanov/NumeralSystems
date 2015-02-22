46363e390aa91b7eede3764b90efcf1cd808a250
dimanov <dimanov@gmail.com>
2015-02-23 00:07:24 +0200

    Bin2Hex Fixed

--- 06. BinaryToHexadecimal/BinToHex.cs
+++ 06. BinaryToHexadecimal/BinToHex.cs
@@ -22,54 +22,45 @@ class BinToHex
 
     private static void CovertBinToHex(string strNumber)
     {
-        //var digitsList = strNumber.Select(digit => (digit.ToString())).ToList();
         int addZero = strNumber.Length % 4;
 
-        string reversedNumbner = new string(strNumber.Reverse().ToArray());
-        Console.WriteLine(reversedNumbner);
+        StringBuilder sb = new StringBuilder(strNumber);
 
-        StringBuilder sb = new StringBuilder(reversedNumbner);
+        while (addZero > 0)
+        {
+            sb.Insert(0, '0');
+            addZero--;
+        }
 
-        var parsedBin = sb.Append('0', addZero);
-
-        Console.WriteLine(parsedBin);
         var binNumber = new List<string>();
-        var hexNumber = new Stack<string>();
-        //foreach (var digit in strNumber)
-        //{
-        //    Console.Write(digit);
-        //}
+        var hexNumber = new Queue<string>();
 
-         
-        for (var i = 0; i < parsedBin.Length - 3; i += 4)
+        for (var i = 0; i < sb.Length - 3; i += 4)
         {
-            binNumber.Add(parsedBin.ToString().Substring(i, 4));
+            binNumber.Add(sb.ToString().Substring(i, 4));
         }
-        foreach (var digit in binNumber)
-        {
-            Console.WriteLine(digit);
-        }
+
         bool binDigit = true;
         for (int i = 0; i < binNumber.Count; i++)
         {
             switch (binNumber[i])
             {
-                case "0000": hexNumber.Push("0"); break;
-                case "0001": hexNumber.Push("1"); break;
-                case "0010": hexNumber.Push("2"); break;
-                case "0011": hexNumber.Push("3"); break;
-                case "0100": hexNumber.Push("4"); break;
-                case "0101": hexNumber.Push("5"); break;
-                case "0110": hexNumber.Push("6"); break;
-                case "0111": hexNumber.Push("7"); break;
-                case "1000": hexNumber.Push("8"); break;
-                case "1001": hexNumber.Push("9"); break;
-                case "1010": hexNumber.Push("A"); break;
-                case "1011": hexNumber.Push("B"); break;
-                case "1100": hexNumber.Push("C"); break;
-                case "1101": hexNumber.Push("D"); break;
-                case "1110": hexNumber.Push("E"); break;
-                case "1111": hexNumber.Push("F"); break;
+                case "0000": hexNumber.Enqueue("0"); break;
+                case "0001": hexNumber.Enqueue("1"); break;
+                case "0010": hexNumber.Enqueue("2"); break;
+                case "0011": hexNumber.Enqueue("3"); break;
+                case "0100": hexNumber.Enqueue("4"); break;
+                case "0101": hexNumber.Enqueue("5"); break;
+                case "0110": hexNumber.Enqueue("6"); break;
+                case "0111": hexNumber.Enqueue("7"); break;
+                case "1000": hexNumber.Enqueue("8"); break;
+                case "1001": hexNumber.Enqueue("9"); break;
+                case "1010": hexNumber.Enqueue("A"); break;
+                case "1011": hexNumber.Enqueue("B"); break;
+                case "1100": hexNumber.Enqueue("C"); break;
+                case "1101": hexNumber.Enqueue("D"); break;
+                case "1110": hexNumber.Enqueue("E"); break;
+                case "1111": hexNumber.Enqueue("F"); break;
                 default: Console.WriteLine("Not a valid binary number"); binDigit = false; break;
             }
             if (!binDigit)
@@ -80,10 +71,13 @@ class BinToHex
 
         Console.Write("The hexadecimal representation of the number is: ");
 
-
-        foreach (var item in hexNumber)
+        while (true)
         {
-            Console.Write(item);
+            if (hexNumber.Count == 0)
+            {
+                break;
+            }
+            Console.Write(hexNumber.Dequeue());
         }
     }
 }
